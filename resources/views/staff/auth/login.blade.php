73e9839c44883991fa32ecda39e1ad142a783c14
Gabreil llanes <gabriel@ecolohosting.com>
2021-05-13 19:07:00 +0000

    set second lenguage

--- resources/views/staff/auth/login.blade.php
+++ resources/views/staff/auth/login.blade.php
@@ -1,7 +1,9 @@
-@extends('layouts.app')
-
+@extends('staff.layouts.auth')
+@section('title')
+    {{ __('Login') }}
+@endsection
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -66,6 +68,47 @@
                     </form>
                 </div>
             </div>
+        </div>
+    </div>
+</div> --}}
+<div class="limiter">
+    <div class="container-login100 page-background">
+        <div class="wrap-login100">
+            <form class="login100-form" method="POST" action="{{ route('staff.login') }}">
+                @csrf
+                <span class="login100-form-logo">
+                    <img alt="" src="{{ asset('staffFiles/assets/img/hospital.png') }}">
+                </span>
+                <span class="login100-form-title p-b-34 p-t-27">
+                    {{ __('Login') }}
+                </span>
+                <div class="wrap-input100" data-validate = "Enter username">
+                    <input class="input100" type="text" autocomplete="off" name="login" placeholder="{{ __('Username or Email') }}">
+                    <span class="focus-input100" data-placeholder="&#xf207;"></span>
+                </div>
+                <div class="wrap-input100" data-validate="Enter password">
+                    <input class="input100" type="password" autocomplete="off" name="password" placeholder="{{ __('Password') }}">
+                    <span class="focus-input100" data-placeholder="&#xf191;"></span>
+                </div>
+                <div class="contact100-form-checkbox">
+                    <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember">
+                    <label class="label-checkbox100" for="ckb1">
+                        {{ __('Remember Me') }}
+                    </label>
+                </div>
+                <div class="container-login100-form-btn">
+                    <button class="login100-form-btn" type="submit">
+                        {{ __('Login') }}
+                    </button>
+                </div>
+                @if (Route::has('password.request'))
+                   <div class="text-center p-t-30">
+                       <a class="txt1" href="forgot_password.html">
+                           {{ __('Forgot Your Password?') }}
+                       </a>
+                   </div>
+                @endif
+            </form>
         </div>
     </div>
 </div>
---
+++ resources/views/staff/layouts/auth.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta name="description" content="Responsive Admin Template" />
+    <meta name="author" content="RedstarHospital" />
+    <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
+     <!-- google font -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet" type="text/css" />
+    <!-- icons -->
+    <link href="{{ asset('staffFiles/fonts/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css"/>
+    <!-- <link rel="stylesheet" href="../assets/plugins/iconic/css/material-design-iconic-font.min.css"> -->
+    <link rel="stylesheet" href="{{ asset('staffFiles/cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css') }}">
+    <!-- bootstrap -->
+    <link href="{{ asset('staffFiles/assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- style -->
+    <link rel="stylesheet" href="{{ asset('staffFiles/assets/css/pages/extra_pages.css') }}">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="http://radixtouch.in/templates/admin/sunray/source/assets/img/favicon.ico" />
+</head>
+<body>
+    @yield('content')
+    <!-- start js include path -->
+     <script src="{{ asset('staffFiles/assets/plugins/jquery/jquery.min.js') }}" ></script>
+    <!-- bootstrap -->
+    <script src="{{ asset('staffFiles/assets/plugins/bootstrap/js/bootstrap.min.js') }}" ></script>
+    <script src="{{ asset('staffFiles/assets/js/pages/extra_pages/extra_pages.js') }}"></script>
+    <!-- end js include path -->
+</body>
+</html>
