dec73561c55498f4b66b7bcd1ef3dbd9627cc871
Gabriel <tejeda.llanes@gmail.com>
2022-08-19 15:54:36 -0700

    fix price 22

--- app/Http/Controllers/Staff/TreatmentController.php
+++ app/Http/Controllers/Staff/TreatmentController.php
@@ -104,7 +104,7 @@ class TreatmentController extends Controller
                     return $treatment->service->service;
                 })
                 ->addColumn('procedure', function($treatment){
-                    return $treatment->procedure;
+                    return $treatment->procedure->procedur;
                 })
                 ->addColumn('package', function($treatment){
                     if (!is_null($treatment->package_id)) {
