5577fd6a97c96bd03d82ea3806e88dc16e3b271a
Gabriel <tejeda.llanes@gmail.com>
2023-01-09 16:24:39 -0800

    add socket8

--- prado.js
+++ prado.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
+const server = require('https').createServer(app);
 // var Redis = require('ioredis');
 // var redis = new Redis();
 var users = [];
