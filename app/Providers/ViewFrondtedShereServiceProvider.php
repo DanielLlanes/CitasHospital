d203fa11313bf489ca391f8e8f41d636865e13a6
Gabriel <tejeda.llanes@gmail.com>
2023-01-19 12:31:32 -0800

    fix

--- app/Providers/ViewFrondtedShereServiceProvider.php
+++ app/Providers/ViewFrondtedShereServiceProvider.php
@@ -40,75 +40,75 @@ class ViewFrondtedShereServiceProvider extends ServiceProvider
             
         // }
     
-        view()->composer('*', function ($view) 
-        {
-            date_default_timezone_set('America/Tijuana');
-            $lang = app()->getLocale();
-            $lang = ($lang == 'es') ? 'es' : "en";
-            $brands = [];
-            $coordinatorFooter = [];
-            $debateMessages = [];
-            $notifications = [];
-            if (isRoleExist('coordinator')) {
-                $coordinatorFooter = Staff::role('coordinator')
-                ->with
-                    (
-                        [
-                            'assignToService' => function($q) use($lang) 
-                            {
-                                $q->selectRaw("services.id, service_$lang as service, brand_id");
-                                $q->with
-                                (
-                                    [
-                                        'brand'
-                                    ]
-                                );
-                            }
-                        ]
-                    )
-                ->get();
+        // view()->composer('*', function ($view) 
+        // {
+        //     date_default_timezone_set('America/Tijuana');
+        //     $lang = app()->getLocale();
+        //     $lang = ($lang == 'es') ? 'es' : "en";
+        //     $brands = [];
+        //     $coordinatorFooter = [];
+        //     $debateMessages = [];
+        //     $notifications = [];
+        //     if (isRoleExist('coordinator')) {
+        //         $coordinatorFooter = Staff::role('coordinator')
+        //         ->with
+        //             (
+        //                 [
+        //                     'assignToService' => function($q) use($lang) 
+        //                     {
+        //                         $q->selectRaw("services.id, service_$lang as service, brand_id");
+        //                         $q->with
+        //                         (
+        //                             [
+        //                                 'brand'
+        //                             ]
+        //                         );
+        //                     }
+        //                 ]
+        //             )
+        //         ->get();
 
-                $brands = Brand::select("*")
-                ->whereHas
-                    (
-                        'service', function($q)
-                        {
-                            $q->whereNotNull('id');
-                        },
-                    )
-                ->with
-                    (
-                        [
-                            'imageOne',
-                            'service' => function($q) use ($lang){
-                                $q->select(["id", "brand_id", "service_$lang AS service"])
-                                ->with('procedures');
-                            },
-                        ]
-                    )
-                ->get();
+        //         $brands = Brand::select("*")
+        //         ->whereHas
+        //             (
+        //                 'service', function($q)
+        //                 {
+        //                     $q->whereNotNull('id');
+        //                 },
+        //             )
+        //         ->with
+        //             (
+        //                 [
+        //                     'imageOne',
+        //                     'service' => function($q) use ($lang){
+        //                         $q->select(["id", "brand_id", "service_$lang AS service"])
+        //                         ->with('procedures');
+        //                     },
+        //                 ]
+        //             )
+        //         ->get();
 
-                if (Auth::guard('staff')->check()) {
-                    $lang = Auth::guard('staff')->user()->lang;
-                    app()->setLocale($lang);
-                    $debateMessages = Message::with([
-                        'debateInverseMessages' => function($q)
-                        {
-                            $q->with('staffDebate');
-                        }
-                    ])
-                    ->orderBy('created_at', 'DESC')
-                    ->where('staff_id', Auth::guard('staff')->user()->id)
-                    ->get();
+        //         if (Auth::guard('staff')->check()) {
+        //             $lang = Auth::guard('staff')->user()->lang;
+        //             app()->setLocale($lang);
+        //             $debateMessages = Message::with([
+        //                 'debateInverseMessages' => function($q)
+        //                 {
+        //                     $q->with('staffDebate');
+        //                 }
+        //             ])
+        //             ->orderBy('created_at', 'DESC')
+        //             ->where('staff_id', Auth::guard('staff')->user()->id)
+        //             ->get();
 
-                    $notifications = Notification::orderBy('created_at', 'DESC')
-                    ->where('staff_id', Auth::guard('staff')->user()->id)
-                    ->with('notificationStaff')
-                    ->get();
-                }
+        //             $notifications = Notification::orderBy('created_at', 'DESC')
+        //             ->where('staff_id', Auth::guard('staff')->user()->id)
+        //             ->with('notificationStaff')
+        //             ->get();
+        //         }
                 
-            }
-            $view->with(['brands' => $brands, 'coordinatorFooter' => $coordinatorFooter, 'debateMessages' => $debateMessages, 'notifications' => $notifications]);    
-        }); 
+        //     }
+        //     $view->with(['brands' => $brands, 'coordinatorFooter' => $coordinatorFooter, 'debateMessages' => $debateMessages, 'notifications' => $notifications]);    
+        // }); 
     }
 }
