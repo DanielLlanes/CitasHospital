0bfe458512a07d8acc9801a227ac350643909dbb
Gabriel <tejeda.llanes@gmail.com>
2023-06-07 10:02:13 -0700

    test email 2

--- routes/staff.php
+++ routes/staff.php
@@ -358,9 +358,8 @@ Route::name('staff.')->namespace('Staff')->group(function(){
             $data = array('name'=>"Virat Gandhi");
    
             Mail::send(['text'=>'staff/mail/test'], $data, function($message) {
-                $message->to('abc@gmail.com', 'Tutorials Point')->subject
+                $message->to('gabriel@jlpradosc.com', 'GAbriel Point')->subject
                     ('Laravel Basic Testing Mail');
-                $message->from('xyz@gmail.com','Virat Gandhi');
             });
             echo "Basic Email Sent. Check your inbox.";
         });
