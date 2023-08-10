cfdb4f2253fc6675aa07ccf64178ecba1088d6c5
Gabriel <tejeda.llanes@gmail.com>
2023-08-10 10:38:13 -0700

    update sup8

--- app/Http/Controllers/Staff/DashboardController.php
+++ app/Http/Controllers/Staff/DashboardController.php
@@ -89,7 +89,7 @@ class DashboardController extends Controller
         ->take(5)
         ->get();
 
-       return $apps[0]->treatment;
+       return $apps[0]->treatment->brand;
 
         $lang = Auth::guard('staff')->user()->lang;
         $lang = app()->getLocale();
